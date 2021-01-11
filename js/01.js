007d7f568c3c36a8591ff66565e7538c7c9f48d3
fernando <fernandounah.rodriguez@gmail.com>
2021-01-11 17:13:29 -0600

    Subiendo el inicio del curso

---
+++ js/01.js
@@ -0,0 +1,16 @@
+//Variables con VAR
+//NOTA: ";" no es obligatorio ponerlo, en JS es opcional
+var producto = 'Audifonos Gamer'; //iniciar variable y asignarle valor
+var disponible; //También se pueden crear variables sin asignarle valor
+//producto = true; //Reasignando el valor de la variable.
+
+var producto1 = 'Computadora',
+disponible1 = true,
+categoria = 'Computadoras'; //Inicializar multiples varibles para evitar poner tantas veces VAR
+
+//Estilos para las variables
+var nombre_producto = 'Monitor HD'; //Underscore
+var nombreProducto = 'Monitor HD'; //Camelcase
+var NombreProducto = 'Monitor HD'; //Pascal case
+
+console.log(producto);
---
+++ js/02.js
@@ -0,0 +1,15 @@
+//Variables con let
+
+//NOTA: ";" no es obligatorio ponerlo, en JS es opcional
+let producto = 'Audifonos Gamer'; //iniciar variable y asignarle valor
+let disponible; //También se pueden crear variables sin asignarle valor
+//producto = true; //Reasignando el valor de la variable.
+
+let producto1 = 'Computadora',
+disponible1 = true,
+categoria = 'Computadoras'; //Inicializar multiples varibles para evitar poner tantas veces VAR
+
+//Estilos para las variables
+let nombre_producto = 'Monitor HD'; //Underscore
+let nombreProducto = 'Monitor HD'; //Camelcase
+let NombreProducto = 'Monitor HD'; //Pascal case
